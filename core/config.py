cb9c45bece576fee075737d9f51e550797cd9d56
AttorneyTao <taoye@grandall.com.cn>
2025-07-31 10:45:04 +0800

    fix some bug

--- core/config.py
+++ core/config.py
@@ -1,6 +1,7 @@
 import os
 import yaml
-
+from dotenv import load_dotenv
+load_dotenv()
 GEMINI_CONFIG = {
     "api_key": os.getenv("GEMINI_API_KEY"),
     "model": "gemini-2.5-flash-preview-05-20"
